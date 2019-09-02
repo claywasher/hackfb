422d7aa5821f4ef20e10b5194a75116b3d278829
claywasher <46997038+claywasher@users.noreply.github.com>
2019-09-02 16:42:14 +0700

    Create install-bahan.sh

---
+++ install-bahan.sh
@@ -0,0 +1,12 @@
+echo "[+] PROSES INSTALL" | lolcat
+figlet ".. FANS C2 .. " | lolcat
+pip2 install beautifulsoup4
+pkg update && pkg upgrade
+npm install -g bash-obfuscate
+pkg install python
+pkg install ruby
+pkg install cowsay
+gem install lolcat
+pip2 install os
+pip2 install sys
+pip2 install mechanize
